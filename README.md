a8312873598fe7584170fa4c3423fa20a868a5e6
Tracy Ma <1991md@gmail.com>
2019-11-28 13:01:59 +0800

    add std::to_chars

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 	vcpkg install benchmark
 	vcpkg install corrade
 	vcpkg install fmt
+
+
+# Result
+
+	11/28/19 12:49:28
+	Running C:\workspace\format-benchmark\build\Release\main.exe
+	Run on (6 X 2904 MHz CPU s)
+	CPU Caches:
+	  L1 Data 32K (x6)
+	  L1 Instruction 32K (x6)
+	  L2 Unified 262K (x6)
+	  L3 Unified 9437K (x1)
+	-----------------------------------------------------
+	Benchmark           Time             CPU   Iterations
+	-----------------------------------------------------
+	BM_fmt            845 ns          785 ns       896000
+	BM_corrade       2478 ns         2455 ns       280000
+	BM_tochars        247 ns          241 ns      2986667
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <fmt/format.h>
 #include <Corrade/Utility/FormatStl.h>
+#include <array>
 
 void fmt_fmt() {
 	for(int i=0; i<10; ++i) {
@@ -11,6 +12,13 @@ void fmt_fmt() {
 void fmt_corrade() {
 	for(int i=0; i<10; ++i) {
 		auto str = Corrade::Utility::formatString("index{}", i);	
+	}
+}
+
+void fmt_tochars() {
+	for(int i=0; i<10; ++i) {
+		std::array<char, 10> dest{};
+		auto res = Corrade::Utility::formatString(dest.data(), dest.size(), i);	
 	}
 }
 
@@ -26,7 +34,14 @@ static void BM_corrade(benchmark::State& state) {
 	}
 }
 
+static void BM_tochars(benchmark::State& state) {
+	for(auto _ : state) {
+		fmt_tochars();
+	}
+}
+
 BENCHMARK(BM_fmt);
 BENCHMARK(BM_corrade);
+BENCHMARK(BM_tochars);
 
 BENCHMARK_MAIN();
